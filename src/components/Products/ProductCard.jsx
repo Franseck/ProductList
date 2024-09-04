ea88b43fe08b8c74af7a4f4edd3f4dd434a683d6
Seckin Ozturk <franseck102@gmail.com>
2024-09-03 20:48:30 -0400

    heart update

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import { MdFavorite } from "react-icons/md";
 
 const ProductCard = ({ image, price, title }) => {
+  const[heart, setHeart]=useState(false)
+  const handleFavorite =()=>{setHeart(!heart)}
   return (
     <div>
       <div className="card">
+            
         <div className="price">
+     <MdFavorite className={heart ? "heartRed": ""} size={30} onClick={handleFavorite} />
          <h3>{price}</h3>
+
 </div>
+
         <img src={image} alt="" />
        <div className="card-title">
           <p>{title}</p>
